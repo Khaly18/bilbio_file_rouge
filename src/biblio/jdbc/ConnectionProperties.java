1d525f216ca3c67e0a846ada2f79826d48cb7bfe
John-Weetaker <jjdormevil@gmail.com>
2016-05-31 23:24:17 +0200

    + add ConnectionProperties file

---
+++ src/biblio/jdbc/ConnectionProperties.java
@@ -0,0 +1,79 @@
+package biblio.jdbc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author John-Weetaker
+ *
+ */
+public class ConnectionProperties {
+	private static final Properties connJDBC = new Properties();
+	private static Connection conn =null;
+	
+	private ConnectionProperties(){
+		
+	}
+	public Connection getInstance(){
+		if (conn == null){
+			try {
+				Class.forName(connJDBC.getProperty("driver"));
+				return conn = DriverManager.getConnection(connJDBC.getProperty("url"), 
+						connJDBC.getProperty("user"), connJDBC.getProperty("pwd"));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
+	public static void creatPropertiesFile(String user, String pass, String fileName){
+		connJDBC.setProperty("driver", "oracle.jdbc.driver.OracleDriver");
+		connJDBC.setProperty("url", "jdbc:oracle:thin:@localhost:1521:xe");
+		connJDBC.setProperty("user", user);
+		connJDBC.setProperty("pwd", pass);
+		writeTheFile(fileName);
+	}
+	public static void creatPropertiesFile(String driver, String url, String user, String pass, String fileName){
+		connJDBC.setProperty("driver", driver);
+		connJDBC.setProperty("url", url);
+		connJDBC.setProperty("user", user);
+		connJDBC.setProperty("pwd", pass);
+		writeTheFile(fileName);
+	}
+	public static void loadProperties(String filePath){
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(filePath);
+			connJDBC.load(fis);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	/**
+	 *
+	 *	write the file in the hardDrive with the specified fileName
+	 */
+	private static void writeTheFile(String fileName){
+		try {
+			FileOutputStream outPath = new FileOutputStream(fileName+".properties");
+			connJDBC.store(outPath, null);
+			outPath.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
